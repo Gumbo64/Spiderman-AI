58dab96bb64076e64caf6293a87e1d36dc59cbf5
Gumbo64 <chickennuggetcrazykid@gmail.com>
2022-05-17 07:49:46 +1000

    f

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+
 driver = webdriver.Firefox()
 driver.get("J:\Github repos\Spiderman-AI\spiderman.html")
 time.sleep(1000)
-el=driver.find_elements_by_xpath("/div/canvas")[0]
+# el=driver.find_elements_by_xpath("/div/canvas")[0]
 
 
 while True:
     time.sleep(1000)
-    action = webdriver.common.action_chains.ActionChains(driver)
-    action.move_to_element_with_offset(el, 100, 100)
-    action.click()
-    action.perform()
+    # el=driver.find_elements_by_xpath("/div/canvas")[0]
+    # action = webdriver.common.action_chains.ActionChains(driver)
+    # action.move_to_element_with_offset(el, 100, 100)
+    # action.click()
+    # action.perform()
