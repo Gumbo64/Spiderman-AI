a88443f649788674f00794ec617a8d655bbfd667
Gumbo64 <chickennuggetcrazykid@gmail.com>
2022-05-17 20:37:30 +1000

    start game

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+
+def click_coord(x,y):
+    el=driver.find_element(By.ID,"bigbody")
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(el, x, y)
+    action.click()
+    action.perform()
+
 
 
 driver = webdriver.Firefox()
 driver.get("J:\Github repos\Spiderman-AI\spiderman.html")
-time.sleep(1000)
-# el=driver.find_elements_by_xpath("/div/canvas")[0]
-
+time.sleep(5)
+click_coord(130,280)
 
 while True:
-    time.sleep(1000)
-    # el=driver.find_elements_by_xpath("/div/canvas")[0]
-    # action = webdriver.common.action_chains.ActionChains(driver)
-    # action.move_to_element_with_offset(el, 100, 100)
-    # action.click()
-    # action.perform()
+
+    time.sleep(0.5)
