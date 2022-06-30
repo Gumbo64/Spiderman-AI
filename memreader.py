29aa02124887831c8cbbdbcfb2f72f9252d9c78d
Gumbo64 <chickennuggetcrazykid@gmail.com>
2022-06-30 23:25:42 +1000

    open game, cleaner

--- memreader.py
+++ memreader.py
@@ -1,6 +1,15 @@
 from importlib.util import module_from_spec
+from time import sleep
 from pymem import *
 from pymem.process import *
+
+import os
+import subprocess
+subprocess.Popen(["ruffle.exe", "spiderman.swf"])
+# os.system('cmd /c "./'+os.getcwd()+'./ruffle.exe spiderman.swf"')
+
+
+sleep(4)
 
 mem = Pymem("ruffle.exe")
 
@@ -8,15 +17,6 @@ module = module_from_name(mem.process_handle,"ruffle.exe").lpBaseOfDll
 print(module)
 offsets = [0x190,0x40,0x330,0x90,0x248,0x28,0x530,0x78,0x1B0,0x10]
 offsets.reverse()
-print(offsets)
-
-
-
-# score = mem.read_double(0x139E38317A0)
-# print(0x139E38317A0)
-# print(score)
-
-
 
 def getPtrAddr(address, offsets):
     addr = mem.read_longlong(address)
@@ -27,9 +27,26 @@ def getPtrAddr(address, offsets):
     return addr
 
 
-score_addr = getPtrAddr(module + 0x00A2DB08,offsets)
+def get_score():
+    score_addr = getPtrAddr(module + 0x00A2DB08,offsets)
+    score = mem.read_double(score_addr)
+    return score
 
-
-score = mem.read_double(score_addr)
-
-print(score)
+lastscore = 0
+counter=0
+while True:
+    sleep(0.01)
+    try:
+        score = get_score()
+        if(score == lastscore):
+            raise Exception("a")
+        print(score)
+        print(counter)
+        counter=0
+        
+    except:
+        counter+=1
+        score = lastscore
+        # print(counter)
+    
+    lastscore=score
